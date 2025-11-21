5f4c3e6d243026b25c6678adea76d1a2b982889d
AmirHosein <karimzade.ah@gmail.com>
2025-11-21 19:33:36 +0330

    Auth Middleware Updated.

--- Back/middlewares/auth.js
+++ Back/middlewares/auth.js
@@ -2,14 +2,11 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET || "default_secret";
 
 function authMiddleware(req, res, next) {
-  console.log("🧩 AuthMiddleware triggered for:", req.path);
   const { token } = req.cookies;
-  console.log("token:", token);
   if (!token) return res.status(401).json({ error: "Unauthorized" });
-
   jwt.verify(token, jwtSecret, {}, (err, userData) => {
     if (err) return res.status(403).json({ error: "Invalid token" });
-    req.user = userData; // 👈 اطلاعات کاربر (id, email, name)
+    req.user = userData;
     next();
   });
 }
